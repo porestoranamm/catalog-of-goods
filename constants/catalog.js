c31c788433816f88a673dc4fc7af2f147d5713c1
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-04 21:53:32 +0300

    add array

---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Da kiselechek?',
+        img: 'img/catalog/daKiselechek.jpg',
+        price: 100,
+    },
+    {
+        id: 'el2',
+        name: 'AmojetEto',
+        img: 'img/catalog/AmojetEto.jpg',
+        price: 150,
+    },
+    {
+        id: 'el3',
+        name: 'DumalaSova',
+        img: 'img/catalog/DumalaSova.jpg',
+        price: 0,
+    }
+]
