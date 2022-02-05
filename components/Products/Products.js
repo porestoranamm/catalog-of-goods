90ac7d6a30fc003bd3181f3816bcc3ee643bbec9
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-05 20:18:54 +0300

    add htmlCatalog

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,8 +1,17 @@
 class Products {
 
     render() {
+        let htmlCatalog = '';
         CATALOG.forEach(({id, name, price, img}) => {
-            console.log(id, name, price, img);
+        
+        htmlCatalog += `
+            <li>
+                <span>${name}</span>
+                <img src="${img}" />
+                <span>${price}</span>
+                <button>Добавить в корзину</button>
+            </li>
+        `; 
         })
     }
 }
