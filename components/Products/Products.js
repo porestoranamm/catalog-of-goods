35421e0972b75d54a85f9ceb01dc2e7fa54d4669
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-05 20:13:41 +0300

    create fucntion render

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,8 +1,8 @@
 class Products {
 
     render() {
-        CATALOG.forEach((element) => {
-            console.log(element);
+        CATALOG.forEach(({id, name, price, img}) => {
+            console.log(id, name, price, img);
         })
     }
 }
