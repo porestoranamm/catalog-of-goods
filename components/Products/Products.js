cc0a2679875b0934e793c7cc8cf840c3075c4573
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-06 19:35:34 +0300

    add classes

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -6,10 +6,10 @@ class Products {
         CATALOG.forEach(({id, name, price, img}) => { 
         htmlCatalog += `
             <li class ="products-element">
-                <span>${name}</span>
-                <img src="${img}" />
-                <span>${price}</span>
-                <button>Добавить в корзину</button>
+                <span class="products-element__name">${name}</span>
+                <img class="products-element__img" src="${img}" />
+                <span class="products-element__price">${price}</span>
+                <button class="products-element__btn">Добавить в корзину</button>
             </li>
         `; 
         });
