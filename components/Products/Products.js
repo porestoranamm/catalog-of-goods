bb897c1671f6b53347a75556060c2acd20e1fb04
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-07 19:29:07 +0300

    add emoji

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -8,7 +8,7 @@ class Products {
             <li class ="products-element">
                 <span class="products-element__name">${name}</span>
                 <img class="products-element__img" src="${img}" />
-                <span class="products-element__price">${price}</span>
+                <span class="products-element__price">⚡️${price}</span>
                 <button class="products-element__btn">Добавить в корзину</button>
             </li>
         `; 
