eb0dffda27b263300b9ee6a2d8eee925124de7ee
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-07 19:36:37 +0300

    create code price

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -8,7 +8,9 @@ class Products {
             <li class ="products-element">
                 <span class="products-element__name">${name}</span>
                 <img class="products-element__img" src="${img}" />
-                <span class="products-element__price">⚡️${price}</span>
+                <span class="products-element__price">
+                ⚡️${price.toLocaleString()} RUB
+                </span>
                 <button class="products-element__btn">Добавить в корзину</button>
             </li>
         `; 
