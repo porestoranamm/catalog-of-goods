681b7d3aab818b4a911869695e2eb214eaa48af8
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-13 18:29:15 +0300

    create names class

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,8 +2,8 @@ class Header {
 
     render() {
         const html = `
-            <div class="header-counter">
-                <div class="header-container></div>
+            <div class="header-container>
+                <div class="header-counter></div>
             </div>
         
         `;
