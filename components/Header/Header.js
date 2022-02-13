3a48a942790263caab8b308f419e6ec9805ffa4c
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-13 18:17:50 +0300

    add new class Header

---
+++ components/Header/Header.js
@@ -0,0 +1,14 @@
+class Header {
+
+    render() {
+        const html = `
+            <div class="header-counter">
+            
+            </div>
+        
+        `;
+    }
+
+}
+
+const headerPage = new Header();
