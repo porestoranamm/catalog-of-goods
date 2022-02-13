83611d18617fda7e57bf920bdc873b7d5f84a6c6
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-13 18:40:52 +0300

    add emoji in header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,14 +3,16 @@ class Header {
     render() {
         const html = `
             <div class="header-container>
-                <div class="header-counter></div>
+                <div class="header-counter>
+                🔥77
+                </div>
             </div>
         
         `;
 
         ROOT_HEADER.innerHTML = html;
     }
-
 }
 
 const headerPage = new Header();
+headerPage.render();
