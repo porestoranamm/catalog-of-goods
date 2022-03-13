b5b6ff4a0f2208ce1a7e7a9a804d9852e873942c
Nikita Kuznetsov <livisback@yandex.ru>
2022-03-13 14:05:04 +0300

    add code fetch

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ let CATALOG = [];
 render();
 
 fetch('server/catalog.json')
+    .then(res => res.json())
+    .then(body => {
+        CATALOG = body;
+    })
+    .catch(error => {
+        console.log(error);
+    });
