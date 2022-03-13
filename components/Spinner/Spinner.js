e8facff78f55eb34702d299c09afedfd46647cca
Nikita Kuznetsov <livisback@yandex.ru>
2022-03-13 23:11:09 +0300

    add spinner img

--- components/Spinner/Spinner.js
+++ components/Spinner/Spinner.js
@@ -1,7 +1,11 @@
 class Spinner {
 
     render() {
-        const html = ``;
+        const html = `
+            <div>
+                <img src="components/Spinner/img/spinner.svg" />
+            </div>
+        `;
 
         ROOT_SPINNER.innerHTML = html;
     }
