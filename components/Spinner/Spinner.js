8ee17c38fb1f85d153440580372dcef992fd0a27
Nikita Kuznetsov <livisback@yandex.ru>
2022-03-13 23:03:45 +0300

    new file

--- components/Spinner/Spinner.js
+++ components/Spinner/Spinner.js
@@ -3,6 +3,8 @@ class Spinner {
     render() {
         const html = ``;
 
-        ROOT_SPINNER.innerHTML = html
+        ROOT_SPINNER.innerHTML = html;
     }
 }
+
+const spinnerPage = new Spinner();
