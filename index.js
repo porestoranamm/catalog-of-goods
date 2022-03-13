d356f298f9fceef888dc4bdf30fa9ed05f4b7c26
Nikita Kuznetsov <livisback@yandex.ru>
2022-03-13 14:41:38 +0300

    add render in fetch

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ fetch('server/catalog.json')
     .then(res => res.json())
     .then(body => {
         CATALOG = body;
+        render();
+
     })
     .catch(error => {
         console.log(error);
