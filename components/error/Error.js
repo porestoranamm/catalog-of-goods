754e81d290ff0617775e4306129642fe3755d7bd
Nikita Kuznetsov <livisback@yandex.ru>
2022-03-16 12:45:49 +0300

    add new class eroor

--- components/error/Error.js
+++ components/error/Error.js
@@ -1,10 +1,12 @@
 class Error {
     render () {
         const html = `=
-            <div class="error-message>
+        <div class="error-container">  
+        <div class="error-message>
                 <h3>Нет доступа!</h3>
                 <p>Попробуйте зайти позже</p>
             </div>
+            </div>  
         `;
 
         ROOT_ERROR.innerHTML = html
