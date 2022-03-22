68a24a9e1d3c627775d47a9271c502323f07d683
Nikita Kuznetsov <livisback@yandex.ru>
2022-03-22 19:01:33 +0300

    add code search

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -6,7 +6,13 @@ class Header {
     render(count) {
         const html = `
             <div class="header-container">
-            <div class="header-logo">Memes Shop</div>    
+            <div class="header-logo">Memes Shop</div> 
+            <div class="box-search">
+            <div class"header-search">
+            <input type="search" id="search" placeholder="Поиск..." />
+            <button class="icon"><i class="fa fa-search"></i></button>
+            </div>
+            </div>   
             <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
             🗑 ${count}
                 </div>
