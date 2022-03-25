02512a7f0e6f10a2b1cd3814520b5c542588ec1b
Nikita Kuznetsov <livisback@yandex.ru>
2022-03-25 09:06:00 +0300

    add log search js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -26,3 +26,13 @@ class Header {
 }
 
 const headerPage = new Header();
+
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+searchBtn.classList.toggle("close");
+input.classList.toggle("square");
+};
+
+searchBtn.addEventListener("click", expand);
